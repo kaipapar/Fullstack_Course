901d564570a23000fed1125d53bc05733d5d4886
kaipapar <120021905+kaipapar@users.noreply.github.com>
2023-09-18 11:46:52 +0300

    PT1 EX8

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,15 +1,27 @@
 import { useState } from 'react'
 
+const Statistics = ({good, neutral, bad}) => {
+  const all = good+neutral+bad
+  const average = all / 3
+  const positive = good / all *100
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>average: {average}</p>
+      <p>positive: {positive} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const all = good+neutral+bad
-  const average = all / 3
-  const positive = good / all *100
-  
 
   // luo kolme nappia
   //-->linkitä nappien painaukset tapahtumankäsittelijään
@@ -27,13 +39,7 @@ const App = () => {
       <button onClick={() => setBad(bad + 1)}>
         bad
       </button>
-      <h1>statistics</h1>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {all}</p>
-      <p>average: {average}</p>
-      <p>positive: {positive} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
       
     </div>
   )
